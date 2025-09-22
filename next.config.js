048f7330d6dfc46d2af2d87a9c19fea558085bf2
kanomiki <Kano.Miki@zb.MitsubishiElectric.co.jp>
2025-09-22 06:12:35 +0000

    deleted: htmlファイル

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    API_ENDPOINT: 'https://27gdtu5dih.execute-api.ap-northeast-1.amazonaws.com/v1/ConstraintInformationAcquisitionAPIRequest'
-  }
-}
-
-module.exports = nextConfig
